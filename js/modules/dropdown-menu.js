597bd679534be923e36f4e83b3e374363037207f
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-05-02 21:05:12 -0300

    Dropdown menu

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,14 @@
+export default function initDropdownMenu() {}
+
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["click", "touchstart"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.toggle("active");
+}
