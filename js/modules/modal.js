90dcd28f8d9529aa4d0c1d6033c876bee9bcf753
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-05-27 23:13:52 -0300

    refatorando Modal em uma class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,23 +1,36 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
-  
-  if(botaoAbrir && botaoFechar && containerModal) {
-    
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle('ativo');
+export default class initModal {
+  constructor(botaoAbrir, botaoFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+
+  addModalEvent() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.cliqueForaModal);
+  }
+
+  toggleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }
+
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.eventToggleModal(event);
     }
-    function cliqueForaModal(event) {
-      if(event.target === this) {
-        toggleModal(event);
-      }
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvent();
     }
-  
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
   }
 }
-
