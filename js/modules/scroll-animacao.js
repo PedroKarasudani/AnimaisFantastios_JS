d7dae002e2e1693de1486cc38edccb5d6afcdbbb
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-05-30 12:59:41 -0300

    refatorando scroll animacao para class

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,22 +1,24 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+    this.animaScroll = this.animaScroll.bind(this);
+  }
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else if (section.classList.contains("ativo")) {
-          section.classList.remove("ativo");
-        }
-      });
-    }
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
 
-    animaScroll();
-
-    window.addEventListener("scroll", animaScroll);
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
   }
 }
