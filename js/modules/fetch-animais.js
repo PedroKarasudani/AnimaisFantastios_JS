6277b779fb357e53be310eb67724e20d18e5ec84
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-05-29 22:34:33 -0300

    feath animais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,31 +1,36 @@
 import AnimaNumeros from "./anima-numeros.js";
 
-export default function initFetchAnimais() {
+export default function feathAnimais(url, target) {
   function createAnimal(animal) {
-    const numerosGrid = document.querySelector(".numeros-grid");
     const div = document.createElement("div");
     div.classList.add("numero-animal");
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
-    numerosGrid.appendChild(div);
+    return div;
   }
 
-  async function initAsync(url) {
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  }
+
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", "ativo");
+    animaNumeros.init();
+  }
+
+  async function criarAnimais() {
     try {
       const animaisResponse = fetch(url);
       const animaisJSON = await (await animaisResponse).json();
 
-      animaisJSON.forEach((animal) => {
-        createAnimal(animal);
-      });
-      const animaNumeros = new AnimaNumeros(
-        "[data-numero]",
-        ".numeros",
-        "ativo"
-      );
-      animaNumeros.init();
+      animaisJSON.forEach((animal) => preencherAnimais(animal));
+
+      animaAnimaisNumeros();
     } catch (error) {
       console.log(error);
     }
   }
-  initAsync("./animais.json");
+
+  return criarAnimais();
 }
